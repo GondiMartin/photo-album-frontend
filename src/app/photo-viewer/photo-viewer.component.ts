f07af2d8433dc674aea9b5bb7de4e2fc7a6826f9
rvili20 <100083135+rvili20@users.noreply.github.com>
2025-04-03 20:00:08 +0200

    Photos can now be listed

--- src/app/photo-viewer/photo-viewer.component.ts
+++ src/app/photo-viewer/photo-viewer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../models/user";
+import {UserService} from "../services/user-service";
 
 @Component({
   selector: 'app-photo-viewer',
   templateUrl: './photo-viewer.component.html',
   styleUrls: ['./photo-viewer.component.css']
 })
-export class PhotoViewerComponent {
+export class PhotoViewerComponent implements OnInit{
+  user: User = new User();
+
+  constructor(
+      private userService: UserService
+  ) {
+  }
+
+  ngOnInit(): void {
+    const userId: string = JSON.parse(sessionStorage.getItem('current-user-id') || '');
+    this.userService.getUserById(userId).subscribe(user => {
+        this.user = user;
+    })
+  }
+
 
 }
