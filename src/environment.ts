d73ac6098e5364c3a77cdf7492e834e8b70ee30a
martin.gondocs@gmail.com <martin.gondocs@gmail.com>
2025-04-05 17:25:21 +0200

    Change hostname based on [oc get routes]

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: "https://photo-album-backend.mgondocs-dev.svc.cluster.local:8080"
+    API_URL: "photo-album-backend-mgondocs-dev.apps.rm3.7wse.p1.openshiftapps.com:8080"
 }
