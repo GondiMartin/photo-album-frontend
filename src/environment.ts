c398ae17658429c461e4da69aff7764efb6fbd52
martin.gondocs@gmail.com <martin.gondocs@gmail.com>
2025-04-05 17:02:56 +0200

    Change hostname

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: "https://photo-album-backend:8080"
+    API_URL: "https://photo-album-backend.mgondocs-dev.svc.cluster.local:8080"
 }
